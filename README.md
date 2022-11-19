ff0d30efbcdca06d8ab4ede3aa719f8bd789c584
christianrhodes18 <thechristianrhodes@gmail.com>
2022-11-19 16:17:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Tailwind Nextjs Portfolio
+
+This is a personal developer portfolio website. 
+
+This site was made with the help of the Tailwind Nextjs Starter Blog ![https://vercel.com/templates/next.js/tailwind-css-starter-blog](Template) from Vercel. 
---
+++ data/projectsData.ts
@@ -0,0 +1,18 @@
+const projectsData = [
+  {
+    title: 'Old Portfolio Site with Vue.js',
+    description: `Made in March 2022, this site was made with Vue.js. The Deployment, Hosting, and CI/CD is done with Netlify. This site has been replaced as a personal portfolio with the current site. `,
+    imgSrc: '/static/images/Vuejs-portfolio-site.png',
+    href: 'https://github.com/christianrhodes18/PersonalWebsite',
+  },
+  {
+    title: 'Quiz Web App',
+    description: `This quiz application of miscellaneous animal trivia questions is an interactive, responsive 
+    app. This app was made with vanilla javascript, html, and css.
+    Note: GitHub Pages version available`,
+    imgSrc: '/static/images/quiz-app-snapshot.png',
+    href: 'https://github.com/christianrhodes18/Trivia-App', //https://github.com/christianrhodes18/Trivia-App
+  },
+]
+
+export default projectsData
