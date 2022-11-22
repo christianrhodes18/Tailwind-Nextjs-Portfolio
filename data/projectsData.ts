c307f378f000370009f00c43f7733258466d49cc
christianrhodes18 <thechristianrhodes@gmail.com>
2022-11-21 22:19:48 -0500

    removed back tick string

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,23 +1,19 @@
 const projectsData = [
   {
     title: 'Full Stack Marketplace',
-    description: `Built with Python Flask, this full stack marketplace project utilizes AWS for the backend storing info 
-    related to users and products. Users can register / log in, add and remove products from cart, search, and checkout. `,
+    description: 'Built with Python Flask, this full stack marketplace project utilizes AWS for the backend storing info related to users and products. Users can register / log in, add and remove products from cart, search, and checkout. ',
     imgSrc: '/static/images/guitar-marketplace.png',
     href: 'https://github.com/nikhilchoppa/guitors' //https://retro-guitars.herokuapp.com
   },
   {
     title: 'Old Portfolio Site with Vue.js',
-    description: `Made in March 2022, this site was made with Vue.js. The Deployment, Hosting, and CI/CD is done with Netlify. 
-    This site has been replaced as a personal portfolio with the current site. `,
+    description: 'Made in March 2022, this site was made with Vue.js. The Deployment, Hosting, and CI/CD is done with Netlify. This site has been replaced as a personal portfolio with the current site. ',
     imgSrc: '/static/images/Vuejs-portfolio-site.png',
     href: 'https://github.com/christianrhodes18/PersonalWebsite',
   },
   {
     title: 'Quiz Web App',
-    description: `This quiz application of miscellaneous animal trivia questions is an interactive, responsive 
-    app. This app was made with vanilla javascript, html, and css.
-    Note: GitHub Pages version available`,
+    description: 'This quiz application of miscellaneous animal trivia questions is an interactive, responsive app. This app was made with vanilla javascript, html, and css. Note: GitHub Pages version available ',
     imgSrc: '/static/images/quiz-app-snapshot.png',
     href: 'https://github.com/christianrhodes18/Trivia-App', //https://github.com/christianrhodes18/Trivia-App
   },
