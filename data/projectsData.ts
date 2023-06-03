f16e804e45d19789411988255ef11d1219c614ea
Christian Rhodes <christianrhodes@Christians-MBP.attlocal.net>
2023-06-03 14:59:07 -0400

    add project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,22 +1,32 @@
 const projectsData = [
   {
+    title: 'Lemono Website',
+    description:
+      'Website for Lemono, a cryptocurrency project. This site is a responsive SPA built with Next.js and Tailwind CSS.',
+    imgSrc: '/static/images/lemono-site.png',
+    hrefLive: 'https://getlemono.com',
+  },
+  {
     title: 'FAMCO Website',
-    description: 'This website was made for FAMCO, a sustainable bitcoin mining company. This site is a responsive SPA built with Next.js and Tailwind CSS.',
+    description:
+      'This website was made for FAMCO, a sustainable bitcoin mining company. This site is a responsive SPA built with Next.js and Tailwind CSS.',
     imgSrc: '/static/images/famco_site.png',
-    hrefLive: 'https://firstargentinemining.co/'
+    hrefLive: 'https://firstargentinemining.co/',
   },
   {
     title: 'AI Assisted Mobile Store Screen',
-    description: 'Mobile AI Marketplace page made with the assistance of Midjourney, Stable Diffusion, and Figma. This project is a proof of concept for AI-assisted development, from design to implementation. ',
+    description:
+      'Mobile AI Marketplace page made with the assistance of Midjourney, Stable Diffusion, and Figma. This project is a proof of concept for AI-assisted development, from design to implementation. ',
     imgSrc: '/static/images/mobile-marketplace-ai.png',
     hrefSrc: 'https://github.com/christianrhodes18/mobile-market-ai',
-    hrefLive: 'https://christianrhodes18.github.io/mobile-market-ai/'
+    hrefLive: 'https://christianrhodes18.github.io/mobile-market-ai/',
   },
   {
     title: 'Luna Sky NFT Marketplace',
-    description: 'This is a large-scale NFT marketplace project that is currently in Beta. I was assigned to designing and implementing a schema, which included relationships like social interactions, account management, and Web3 metrics. I also integrated user profile APIs using GraphQL and Apollo. ',
+    description:
+      'This is a large-scale NFT marketplace project that is currently in Beta. I was assigned to designing and implementing a schema, which included relationships like social interactions, account management, and Web3 metrics. I also integrated user profile APIs using GraphQL and Apollo. ',
     imgSrc: '/static/images/luna-sky.png',
-    hrefLive: 'https://www.lunasky.com/'
+    hrefLive: 'https://www.lunasky.com/',
   },
   // {
   //   title: 'Movie TV Tracker App',
@@ -26,43 +36,49 @@ const projectsData = [
   // },
   {
     title: 'Daylight Apollo',
-    description: "I made this landing page for Daylight Farms (a crytpocurrency project) for Dappd, a Web3 SaaS company I work closely with as an independent contractor. This responsive site was over several iterations as Daylight's designers found the best design for their project. " ,
+    description:
+      "I made this landing page for Daylight Farms (a crytpocurrency project) for Dappd, a Web3 SaaS company I work closely with as an independent contractor. This responsive site was over several iterations as Daylight's designers found the best design for their project. ",
     imgSrc: '/static/images/daylight_apollo.png',
-    hrefLive: 'https://ignite.apollolaunch.tech/'
+    hrefLive: 'https://ignite.apollolaunch.tech/',
   },
   {
     title: 'OPTX Website',
-    description: 'I made this website for OPTX (a crytpocurrency project) as a contract for Dappd, a Web3 SaaS company. This responsive site was built from scratch from a Figma design using Next.js and Tailwind CSS. ',
+    description:
+      'I made this website for OPTX (a crytpocurrency project) as a contract for Dappd, a Web3 SaaS company. This responsive site was built from scratch from a Figma design using Next.js and Tailwind CSS. ',
     imgSrc: '/static/images/optx.png',
-    hrefLive: 'https://optx.money/'
+    hrefLive: 'https://optx.money/',
   },
   {
     title: 'Full Stack Marketplace',
-    description: 'Built with Python Flask, this full stack marketplace project utilizes AWS for the backend storing info related to users and products. Users can register / log in, add and remove products from cart, search, and checkout. ',
+    description:
+      'Built with Python Flask, this full stack marketplace project utilizes AWS for the backend storing info related to users and products. Users can register / log in, add and remove products from cart, search, and checkout. ',
     imgSrc: '/static/images/guitar-marketplace.png',
     hrefSrc: 'https://github.com/nikhilchoppa/guitors',
-    hrefLive: 'https://retro-guitars.herokuapp.com'
+    hrefLive: 'https://retro-guitars.herokuapp.com',
   },
   {
     title: 'Three.js Interactive Walkthrough',
-    description: 'JavaScript application using the WebGL library Three.js demonstrating 3D scenes, cameras, lights, and advanced materials. This demo includes starter code to get started with Three.js.',
+    description:
+      'JavaScript application using the WebGL library Three.js demonstrating 3D scenes, cameras, lights, and advanced materials. This demo includes starter code to get started with Three.js.',
     imgSrc: '/static/images/threejs_demo.png',
     hrefSrc: 'https://github.com/christianrhodes18/three-js-galaxy',
-    hrefLive: 'https://three-js-galaxy-mu.vercel.app/'
+    hrefLive: 'https://three-js-galaxy-mu.vercel.app/',
   },
   {
     title: 'Old Portfolio Site with Vue.js',
-    description: 'Made in March 2022, this site was made with Vue.js. The Deployment, Hosting, and CI/CD is done with Netlify. This site has been replaced as a personal portfolio with the current site. ',
+    description:
+      'Made in March 2022, this site was made with Vue.js. The Deployment, Hosting, and CI/CD is done with Netlify. This site has been replaced as a personal portfolio with the current site. ',
     imgSrc: '/static/images/Vuejs-portfolio-site.png',
     hrefSrc: 'https://github.com/christianrhodes18/PersonalWebsite',
-    hrefLive: 'https://christian-rhodes-old.netlify.app/'
+    hrefLive: 'https://christian-rhodes-old.netlify.app/',
   },
   {
     title: 'Quiz Web App',
-    description: 'This quiz application of miscellaneous animal trivia questions is an interactive, responsive app. This app was made with vanilla javascript, html, and css. Note: GitHub Pages version available ',
+    description:
+      'This quiz application of miscellaneous animal trivia questions is an interactive, responsive app. This app was made with vanilla javascript, html, and css. Note: GitHub Pages version available ',
     imgSrc: '/static/images/quiz-app-snapshot.png',
     hrefSrc: 'https://github.com/christianrhodes18/Trivia-App',
-    hrefLive: 'https://christianrhodes18.github.io/Trivia-App/'
+    hrefLive: 'https://christianrhodes18.github.io/Trivia-App/',
   },
 ]
 
