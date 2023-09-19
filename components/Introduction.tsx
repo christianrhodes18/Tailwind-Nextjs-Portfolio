8b3a796384d48a0ed3c1a66a49a4cddde45ef3c6
Christian Rhodes <thechristianrhodes@gmail.com>
2023-09-19 01:10:37 -0600

    made irrelvenet things smaller or gone

--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -7,15 +7,16 @@ const Introduction = () => {
   return (
     <>
       {/* intro section */}
-      <section className="md:flex w-full py-20">
-        <div className="flex-1 basis-1/2">
+      <section className="md:flex w-full py-4 md:py-20">
+        <div className="flex-1 basis-3/4">
           <div className="flex-1 text-left">
             {/* Gradient Text */}
             {/* <h1 className="text-5xl font-bold text-transparent bg-clip-text 
                             bg-gradient-to-r from-blue-600 via-rose-300 to-orange-300 pt-4 md:pt-12">
                             Hi. I'm Christian.
                         </h1> */}
-            <h1 className="text-5xl font-bold">Hi. I'm Christian</h1>
+            <h1 className="text-5xl font-bold">Hi,</h1>
+            <h1 className="text-5xl font-bold">I'm Christian</h1>
             <h2
               className="text-4xl font-medium pt-4 text-transparent bg-clip-text 
                             bg-gradient-to-r from-blue-600 via-rose-300 to-orange-300"
@@ -37,7 +38,7 @@ const Introduction = () => {
             </div>
           </div>
         </div>
-        <div className="flex-2 flex basis-1/2 justify-center">
+        <div className="flex-2 md:flex basis-1/4 justify-center hidden h-1/2">
           <Image
             alt="Animation"
             src={profileImage}
@@ -54,9 +55,9 @@ const Introduction = () => {
       </section>
 
       {/* link cards section */}
-      <section className="flex flex-col md:flex-row gap-8 text-white">
+      <section className="flex flex-col md:flex-row gap-8 text-white w-3/5 max-h-[600px] mx-auto md:w-full">
         <Link href="/projects">
-          <div className="relative cursor-pointer w-full bg-[url('../public/static/images/MyWorkImage.jpeg')] bg-cover h-[500px]">
+          <div className="relative cursor-pointer w-full bg-center bg-[url('../public/static/images/MyWorkImage.jpeg')] bg-cover h-[500px]">
             <h3 className="uppercase text-center absolute inset-x-0 bottom-8 font-medium text-4xl drop-shadow-lg">
               My Work
             </h3>
