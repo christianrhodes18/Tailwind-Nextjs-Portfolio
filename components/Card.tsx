95d4a87aa9395e2c7abfd0de29d2a70d8073cfc9
Christian Rhodes <thechristianrhodes@gmail.com>
2023-09-19 15:43:58 -0600

    add spacing for mobile buttons

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,39 +2,37 @@ import Image from './Image'
 import Link from './Link'
 
 const renderImage = (title: string, imgSrc: string, hrefSrc: string, hrefLive: string) => {
-  console.log("Card: ", Card)
+  console.log('Card: ', Card)
   if (imgSrc) {
     if (hrefSrc) {
       return (
         <Link href={hrefSrc} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center md:h-36 lg:h-48 hover:scale-110 ease-in duration-300 "
-              width={544}
-              height={306}
-            />
-          </Link>
+          <Image
+            alt={title}
+            src={imgSrc}
+            className="object-cover object-center md:h-36 lg:h-48 hover:scale-110 ease-in duration-300 "
+            width={544}
+            height={306}
+          />
+        </Link>
       )
-    }
-    else if (hrefLive) {
+    } else if (hrefLive) {
       return (
         <Link href={hrefLive} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center md:h-36 lg:h-48 hover:scale-110 ease-in duration-300 "
-              width={544}
-              height={306}
-            />
-          </Link>
+          <Image
+            alt={title}
+            src={imgSrc}
+            className="object-cover object-center md:h-36 lg:h-48 hover:scale-110 ease-in duration-300 "
+            width={544}
+            height={306}
+          />
+        </Link>
       )
     }
+  } else {
+    console.log('No image found')
+    return
   }
-    else {
-      console.log("No image found")
-      return
-    }
 }
 
 const Card = ({ title, description, imgSrc, hrefSrc, hrefLive }) => (
@@ -44,7 +42,6 @@ const Card = ({ title, description, imgSrc, hrefSrc, hrefLive }) => (
         imgSrc && 'h-full'
       }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
-
       {renderImage(title, imgSrc, hrefSrc, hrefLive)}
 
       <div className="p-6">
@@ -58,12 +55,12 @@ const Card = ({ title, description, imgSrc, hrefSrc, hrefLive }) => (
           )}
         </h2>
         <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
-        <div className="flex justify-between">
+        <div className="flex justify-between gap-2">
           {hrefSrc && (
             <Link
               href={hrefSrc}
               // className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-              className="bg-white/90 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+              className="bg-white/90 hover:bg-gray-100 text-gray-800 font-semibold max-[375px]:px-2 py-2 px-4 border border-gray-400 rounded shadow"
               aria-label={`Link to ${title}`}
             >
               View Source Code{/*  &rarr; */}
@@ -73,7 +70,7 @@ const Card = ({ title, description, imgSrc, hrefSrc, hrefLive }) => (
             <Link
               href={hrefLive}
               // className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-              className="bg-white/90 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+              className="bg-white/90 hover:bg-gray-100 text-gray-800 font-semibold max-[375px]:px-2 py-2 px-4 border border-gray-400 rounded shadow"
               aria-label={`Link to ${title}`}
             >
               View Site {/* &rarr; */}
